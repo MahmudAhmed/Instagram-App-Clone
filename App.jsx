e33e942270ccbf167105288c234e0974fb357bad
MahmudAhmed <moe.purplefox@gmail.com>
2020-05-14 23:21:52 -0400

    react-firebase

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 import * as Facebook from "expo-facebook";
-import { f, auth, database } from "./config/config";
+import { f, auth, db } from "./config/config.expo";
 import Login from "./app/screens/form_action"
 
 
@@ -17,6 +17,46 @@ export default class App extends React.Component {
         this.setState({ loggedIn: false });
       }
     })
+
+    this.setData()
+     
+  }
+ 
+  setData() {
+    // db.collection("users")
+    //   .get()
+    //   .then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //       console.log(doc.data());
+    //     });
+    //   });
+
+  // var docRef = db.collection("users").doc("of14CQ7s3U1pFhGyIwql");
+
+  // docRef
+  //   .get()
+  //   .then(function (doc) {
+  //     debugger
+  //     if (doc.exists) {
+  //       console.log("Document data:", doc.data());
+  //     } else {
+  //       // doc.data() will be undefined in this case
+  //       console.log("No such document!");
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error getting document:", error);
+  //   });
+  var citiesRef = db.collection("users");
+
+  citiesRef.add({
+    name: "test",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 860000,
+    regions: ["west_coast", "norcal"],
+  });
   }
 
   registerUser = (email, password) => {
--- config/config.expo.js
+++ config/config.expo.js
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD64mYesSD0AdzHc6fa5CDDO44ClUQagec",
@@ -9,10 +10,11 @@ const firebaseConfig = {
   messagingSenderId: "645777166192",
   appId: "1:645777166192:web:8bf870140621337a0f89b5"
 };
+ 
+// const app = firebase.initializeApp(firebaseConfig); 
+export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
-firebase.initializeApp(firebaseConfig);
-
-export const f = firebase;
-export const database = firebase.database();
+// export const f = firebase;
+export const db = firebase.firestore();
 export const auth = firebase.auth();
-export const storage = firebase.storage();
+// export const storage = firebase.storage(); 
